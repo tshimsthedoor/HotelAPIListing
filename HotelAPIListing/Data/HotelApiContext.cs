96bc1c8861a67179b5b4366387ce5cc7cbb79776
Tresor <thedorcode@gmail.com>
2026-02-14 22:20:51 +0200

    I added the model of the country and I did my migration

--- HotelAPIListing/Data/HotelApiContext.cs
+++ HotelAPIListing/Data/HotelApiContext.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HotelAPIListing.Data;
+using Microsoft.EntityFrameworkCore;
 
 public class HotelApiContext : DbContext
 {
     public HotelApiContext(DbContextOptions options) : base(options)
     {
     }
+
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Country> Countries { get; set; }
 }
